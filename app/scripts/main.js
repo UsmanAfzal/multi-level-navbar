e0a314a3090edbd672a72e87d4f50b8887a89e6b
Usman Afzal <uafzal@living-group.com>
2013-10-31 14:59:32 +0000

    removed app folder

--- app/scripts/main.js
+++
@@ -1,8 +0,0 @@
-$('#main-nav li').hover(function () {
-        'use strict';
-        $(this).children('div').delay(180).fadeIn(100);
-    },
-    function () {
-        'use strict';
-        $(this).children('div').fadeOut(50);
-    });
